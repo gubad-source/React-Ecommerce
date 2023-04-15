3fa650b7e6336d0f7a7b3d477bf0f202d4f4e683
gubad-source <qubadmustafayev@gmail.com>
2023-04-15 19:43:51 +0400

    added

--- my-file/app.js
+++ my-file/app.js
@@ -1,4 +1,12 @@
-async function myFunction() {
-  const response = await fetch("https://fakestoreapi.com/carts");
+var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+console.log(randomColor);
+const num = 12;
+for (let i = 0; i < 12; i++) {
+  let div = document.createElement("div");
+  div.style.width = "30px";
+  div.style.height = "30px";
+  div.style.backgroundColor = "#" + randomColor;
+  div.innerHTML = "ok";
+
+  // document.querySelector(".color").innerHTML = div;
 }
-myFunction();
