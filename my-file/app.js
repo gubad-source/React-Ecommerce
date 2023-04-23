f4504a99eb0b5b4a7d48b0a4c004b67b47539456
gubad-source <qubadmustafayev@gmail.com>
2023-04-23 14:15:32 +0400

    change

--- my-file/app.js
+++ my-file/app.js
@@ -1,12 +1,12 @@
-var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-console.log(randomColor);
-const num = 12;
-for (let i = 0; i < 12; i++) {
-  let div = document.createElement("div");
-  div.style.width = "30px";
-  div.style.height = "30px";
-  div.style.backgroundColor = "#" + randomColor;
-  div.innerHTML = "ok";
+// var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+// console.log(randomColor);
+// const num = 12;
+// for (let i = 0; i < 12; i++) {
+//   let div = document.createElement("div");
+//   div.style.width = "30px";
+//   div.style.height = "30px";
+//   div.style.backgroundColor = "#" + randomColor;
+//   div.innerHTML = "ok";
 
-  // document.querySelector(".color").innerHTML = div;
-}
+//   // document.querySelector(".color").innerHTML = div;
+// }
